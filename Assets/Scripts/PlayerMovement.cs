10cdc01da7c748928051f8161fd58ecb0b3d7ad0
Vsugarev <vsugare1@educacion.navarra.es>
2025-10-24 00:09:39 +0200

    feat: Animaciones basicas del personaje jugable añadidas

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoJugador : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 5;
     public Rigidbody2D rb;
-
+    public Animator animator;
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        
 
-        rb.velocity = new Vector2(horizontal,vertical) * speed;
+        animator.SetFloat("Horizontal", Math.abs(horizontal));
+        animator.SetFloat("Vertical", Math.abs(vertical));
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
     }
 }
